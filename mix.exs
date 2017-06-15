af491e16eddf54c3de94e0c9bd6f838f2d389aa4
Letmecode <daniel@letmeco.de>
2017-06-15 17:42:54 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Webpacker.Mixfile do
 
   def project do
     [app: :webpacker,
-     version: "0.1.6",
+     version: "0.1.7",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
