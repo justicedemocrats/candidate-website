964042187da26948a793beecd244ad44466541c1
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-11-02 10:47:40 -0700

    little analytics

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,28 @@ setTimeout(() => {
   const targetHeight = el.getAttribute('data-height')
   el.style.height = `${targetHeight}%`
 }, 500)
+
+const bind = () => {
+  const ref = getQueryStringValue('ref')
+  if (ref != '') {
+    Array.from(document.querySelectorAll('form'))
+    .filter(f => f.method == 'post')
+    .forEach(f => f.action = f.action + `?ref=${ref}`)
+  }
+}
+
+function getQueryStringValue(key) {
+  return decodeURIComponent(
+    window.location.search.replace(
+      new RegExp(
+        '^(?:.*[&\\?]' +
+          encodeURIComponent(key).replace(/[\.\+\*]/g, '\\$&') +
+          '(?:\\=([^&]*))?)?.*$',
+        'i'
+      ),
+      '$1'
+    )
+  )
+}
+
+bind()
