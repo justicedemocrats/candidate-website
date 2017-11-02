af45a43004266b52e8eece891214e0d49660ddde
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-11-02 01:18:24 -0700

    add domains

--- config/config.exs
+++ config/config.exs
@@ -24,6 +24,13 @@ config :cipher,
 # Cosmic
 config :cosmic, slugs: ["alexandria-ocasio-cortez", "alison-hartson"]
 
+# Domains
+config :candidate_website, domains: %{
+  "alisonhartson.com" => "alison-hartson"
+}
+
+
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- lib/global_opts.ex
+++ lib/global_opts.ex
@@ -1,5 +1,11 @@
 defmodule GlobalOpts do
+  @domains Application.get_env(:candidate_website, :domains)
+
   def get(conn, %{"candidate" => candidate}) do
     [candidate: candidate]
+  end
+
+  def get(conn, _params) do
+    [candidate: @doamins[conn.host]]
   end
 end
