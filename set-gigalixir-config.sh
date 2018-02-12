3d610165fb4e244d062cc530108ff18ac71e1656
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-02-12 13:03:43 -0500

    configuration scripts

---
+++ set-gigalixir-config.sh
@@ -0,0 +1,8 @@
+vars=`cat .env`
+
+for var in $vars
+do
+  splitvar=($(echo $var | tr '=' "\n"))
+  gigalixir set_config jd-livevox ${splitvar[0]} ${splitvar[1]}
+  echo "gigalixir set_config jd-candidate ${splitvar[0]} ${splitvar[1]}"
+done
---
+++ set-local-config.sh
@@ -0,0 +1,8 @@
+vars=`cat .env`
+
+for var in $vars
+do
+  splitvar=($(echo $var | tr '=' "\n"))
+  export ${splitvar[0]}=${splitvar[1]}
+  echo "export ${splitvar[0]}=${splitvar[1]}"
+done
