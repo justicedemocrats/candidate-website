629d3bdda800c09b0622ae755f65c654b78a8d74
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-02-17 20:57:02 -0500

    change host an event url

--- set-gigalixir-config.sh
+++ set-gigalixir-config.sh
@@ -3,6 +3,6 @@ vars=`cat .env`
 for var in $vars
 do
   splitvar=($(echo $var | tr '=' "\n"))
-  gigalixir set_config jd-livevox ${splitvar[0]} ${splitvar[1]}
+  gigalixir set_config jd-candidate ${splitvar[0]} ${splitvar[1]}
   echo "gigalixir set_config jd-candidate ${splitvar[0]} ${splitvar[1]}"
 done
