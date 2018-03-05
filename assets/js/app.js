0c1c6f0388732aa67b7f028a61bd9b55dba67e76
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-03-05 14:37:41 -0500

    fix sidebar scrolling issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,42 @@
-import spa from './spa'
-import Hamburger from './components/hamburger'
-import React from 'react'
-import { render } from 'react-dom'
+import spa from "./spa";
+import Hamburger from "./components/hamburger";
+import React from "react";
+import { render } from "react-dom";
 
-spa.bind.all()
+spa.bind.all();
 
-setTimeout(() => {
-  const el = document.querySelector('#filled-animate')
-  const targetHeight = el.getAttribute('data-height')
-  el.style.height = `${targetHeight}%`
-}, 500)
+// setTimeout(() => {
+//   const el = document.querySelector('#filled-animate')
+//   const targetHeight = el.getAttribute('data-height')
+//   el.style.height = `${targetHeight}%`
+// }, 500)
 
 const bind = () => {
-  const ref = getQueryStringValue('ref')
-  if (ref != '') {
-    Array.from(document.querySelectorAll('form'))
-    .filter(f => f.method == 'post')
-    .forEach(f => f.action = f.action + `?ref=${ref}`)
+  const ref = getQueryStringValue("ref");
+  if (ref != "") {
+    Array.from(document.querySelectorAll("form"))
+      .filter(f => f.method == "post")
+      .forEach(f => (f.action = f.action + `?ref=${ref}`));
   }
-}
+};
 
 function getQueryStringValue(key) {
   return decodeURIComponent(
     window.location.search.replace(
       new RegExp(
-        '^(?:.*[&\\?]' +
-          encodeURIComponent(key).replace(/[\.\+\*]/g, '\\$&') +
-          '(?:\\=([^&]*))?)?.*$',
-        'i'
+        "^(?:.*[&\\?]" +
+          encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") +
+          "(?:\\=([^&]*))?)?.*$",
+        "i"
       ),
-      '$1'
+      "$1"
     )
-  )
+  );
 }
 
-setTimeout(bind, 100)
+setTimeout(bind, 100);
 
-const hamburgerTarget = document.getElementById('mobile-menu')
+const hamburgerTarget = document.getElementById("mobile-menu");
 if (hamburgerTarget) {
-  render(<Hamburger />, hamburgerTarget)
+  render(<Hamburger />, hamburgerTarget);
 }
--- assets/js/components/hamburger.jsx
+++ assets/js/components/hamburger.jsx
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import smoothScroll from "smoothscroll";
 
 export default class Hamburger extends Component {
   state = {
     open: false
-  }
+  };
 
-  openMenu = () => this.setState({ open: true })
-  closeMenu = () => this.setState({ open: false })
+  openMenu = () => this.setState({ open: true });
+  closeMenu = () => this.setState({ open: false });
 
   render() {
-    const { open } = this.state
+    const { open } = this.state;
 
     if (open) {
-      return this.renderOpenMenu()
+      return this.renderOpenMenu();
     } else {
-      return this.renderHamburger()
+      return this.renderHamburger();
     }
   }
 
@@ -25,19 +26,15 @@ export default class Hamburger extends Component {
         <div style={styles.bar.bar} />
         <div style={styles.bar.bar} />
       </div>
-    )
+    );
   }
 
   renderOpenMenu() {
     return (
       <div style={styles.screen} onClick={this.closeMenu}>
         <div style={styles.menu}>
-          <div style={styles.candidate.name}>
-            {window.name}
-          </div>
-          <div style={styles.candidate.district}>
-            {window.district}
-          </div>
+          <div style={styles.candidate.name}>{window.name}</div>
+          <div style={styles.candidate.district}>{window.district}</div>
           {siteMap.map(({ text, action, extraStyles }) => (
             <div
               style={Object.assign(
@@ -52,95 +49,98 @@ export default class Hamburger extends Component {
           ))}
         </div>
       </div>
-    )
+    );
   }
 }
 
 const styles = {
   bar: {
     parent: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
       width: 40,
       height: 30,
       marginRight: 15,
       marginTop: 15,
-      cursor: 'pointer'
+      cursor: "pointer"
     },
 
     bar: {
       width: 40,
       height: 5,
       backgroundColor: window.heroTextColor,
-      borderRadius: '10px'
+      borderRadius: "10px"
     }
   },
 
   screen: {
-    width: '100vw',
-    height: '100vh',
-    backgroundColor: 'rgba(0, 0, 0, .3)'
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "rgba(0, 0, 0, .3)"
   },
 
   menu: {
-    position: 'fixed',
-    height: '100vh',
-    width: '66vw',
+    position: "fixed",
+    height: "100vh",
+    width: "66vw",
     right: 0,
-    backgroundColor: 'black',
+    backgroundColor: "black",
     padding: 20,
-    color: 'white'
+    color: "white"
   },
 
   menuElement: {
     marginTop: 15,
     marginBottom: 15,
-    fontFamily: 'Roboto Condensed',
-    fontSize: 'larger',
-    color: '#aaa'
+    fontFamily: "Roboto Condensed",
+    fontSize: "larger",
+    color: "#aaa"
   },
 
   candidate: {
     name: {
-      fontSize: '30px',
-      fontFamily: 'Roboto Condensed'
+      fontSize: "30px",
+      fontFamily: "Roboto Condensed"
     },
 
     district: {
-      fontSize: '24px',
-      fontFamily: 'Roboto Condensed',
-      color: '#aaa',
+      fontSize: "24px",
+      fontFamily: "Roboto Condensed",
+      color: "#aaa",
       marginBottom: 30
     }
   }
-}
+};
 
 const siteMap = [
   {
-    text: 'Contribute',
+    text: "Contribute",
     action: () => (window.location.href = window.donateUrl),
     extraStyles: {
-      borderBottom: '3px solid white',
-      color: 'white'
+      borderBottom: "3px solid white",
+      color: "white"
     }
   },
   {
-    text: 'About',
+    text: "About",
     action: () =>
       window.aboutEnabled
-        ? (window.location.pathname = '/about')
-        : scrollTo('#why')
+        ? (window.location.pathname = "/about")
+        : smoothScroll(document.querySelector("#why"))
   },
-  { text: 'Issues', action: () => (window.location.pathname = '/issues') },
-  { text: 'Events', action: () => scrollTo('#events') },
-  { text: 'News', action: () => scrollTo('#news') },
+  { text: "Issues", action: () => (window.location.pathname = "/issues") },
   {
-    text: 'Volunteer',
-    action: () => scrollTo('#volunteer'),
+    text: "Events",
+    action: () => smoothScroll(document.querySelector("#events"))
+  },
+  { text: "News", action: () => smoothScroll(document.querySelector("#news")) },
+  {
+    text: "Volunteer",
+    action: () => smoothScroll(document.querySelector("#volunteer")),
     extraStyles: {
-      borderBottom: '3px solid white',
-      color: 'white'
+      borderBottom: "3px solid white",
+      color: "white"
     }
   }
-]
+];
