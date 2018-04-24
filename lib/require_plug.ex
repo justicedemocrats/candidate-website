7a4c7f7b110af77df5c4b7cef7ce6ec79b39f217
Leo Sussan <leo.s@joinreal.com>
2018-04-24 08:57:47 -0400

    Adjustments to Analytics conditional, added Google Tag Manager

--- lib/require_plug.ex
+++ lib/require_plug.ex
@@ -16,6 +16,7 @@ defmodule CandidateWebsite.RequirePlug do
   @optional ~w(
     animation_fill_level target_html hero_text_color before_for_congress
     why_support_picture instagram google_analytics_id linkedin hide_lets
+    google_tag_manager_id
   )
 
   @about_attrs ~w(
