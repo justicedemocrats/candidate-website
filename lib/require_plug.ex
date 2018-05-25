24b3155f583e525c24d047cc4fb01786da1b075f
Leo Sussan <leosussan@gmail.com>
2018-05-25 19:05:08 +0000

    Adjustments to issues & platform chunks

--- lib/require_plug.ex
+++ lib/require_plug.ex
@@ -10,7 +10,7 @@ defmodule CandidateWebsite.RequirePlug do
     why_support_body action_shot quote primary_color highlight_color
     vote_registration_url vote_registration_icon vote_instructions_url
     vote_instructions_icon vote_location_url vote_location_icon header_background_color
-    general_email press_email platform_header signup_prompt
+    general_email press_email platform_header platform_chunk_header signup_prompt
   )
 
   @optional ~w(
