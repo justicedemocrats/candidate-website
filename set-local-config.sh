d25aa223251b3a29f1e78d233bfeb553bb6e569a
Hsm7085 <72146615+Hsm7085@users.noreply.github.com>
2020-10-01 00:19:09 +0530

    Update set-local-config.sh

--- set-local-config.sh
+++ set-local-config.sh
@@ -2,7 +2,7 @@ vars=`cat .env`
 
 for var in $vars
 do
-  splitvar=($(echo $var | tr '=' "\n"))
-  export ${splitvar[0]}=${splitvar[1]}
-  echo "export ${splitvar[0]}=${splitvar[1]}"
+  splitvar=($(echo $var || tr '=' "\n"))
+  export ${splitvar[0]}=${splitvar[1]};
+  echo "export ${splitvar[0]}=${splitvar[1]}";
 done
